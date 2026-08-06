a0f2655ea6abc83283c0cef404a0acd2cc2af345
hrqmartins <hrqmartins129@gmail.com>
2026-08-06 19:49:46 -0300

    feat: Adicionando projeto base spring initializr

---
+++ tranquilo-express/src/main/java/br/com/fiap/tranquilo_express/TranquiloExpressApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tranquilo_express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranquiloExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranquiloExpressApplication.class, args);
+	}
+
+}
---
+++ tranquilo-express/src/test/java/br/com/fiap/tranquilo_express/TranquiloExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tranquilo_express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranquiloExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
